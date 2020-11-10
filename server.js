6b8c72e03ae52fef28ee7d363b12141ce52cfe9c
Jehyun <congmul@hotmail.com>
2020-11-10 14:46:09 -0800

    Add Clear table function

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ var path = require("path");
 const app = express();
 const PORT = process.env.PORT | 3000;
 
+let reservationList = [
+    {
+        number: "1",
+        name: "Jehyun",
+        phoneNumber: "123123123",
+        email: "email@email.com",
+        id: "id"
+    }
+];
+let waitList = [];
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -15,6 +26,21 @@ app.get('/tables', (req, res) => {
 app.get('/reserve', (req, res) => {
     res.sendFile(path.join(__dirname, "reservationForm.html"));
 });
+
+app.get('/api/tables', (req, res) => {
+    return res.json(reservationList);
+});
+
+app.get('/api/waitlist', (req, res) => {
+    return res.json(waitList);
+});
+
+app.post('/api/clear', (req, res) => {
+    reservationList = [];
+    return res.json(reservationList);
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
